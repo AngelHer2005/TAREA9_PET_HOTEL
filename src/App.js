74aac46f88ece22ed50f11a472ebb35dd911c0d6
Ángel Hernán Alberto Patricio Arroyo <angelhernanpatricioarroyo@gmail.com>
2025-04-27 19:38:59 -0500

    Add React and MySQL DataBase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './views/Login';
@@ -18,3 +19,30 @@ function App() {
 }
 
 export default App;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 34f6684 (Initialize project using Create React App)
